b83af9e80665b955e4a45fe50386a91abaea4a94
ilyasmuhlisin <21ilyasmhlsn@gmail.com>
2023-06-12 17:43:52 +0700

    sample ui

---
+++ src/components/cardComponent.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+function cardComponent({ movie }) {
+  return (
+    <div className="max-w-sm rounded overflow-hidden shadow-lg">
+      <img className="w-full" src={movie.Poster} alt={movie.Title} />
+      <div className="px-6 py-4">
+        <div className="font-bold text-xl mb-2">{movie.Title}</div>
+        {/* <p className="text-gray-700 text-base">{movie.description}</p> */}
+      </div>
+      <div className="px-6 py-4">
+        {/* <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">
+          {movie.genre}
+        </span> */}
+        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700">
+          {movie.Year}
+        </span>
+      </div>
+    </div>
+  );
+}
+
+export default cardComponent;
