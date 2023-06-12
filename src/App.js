ebc530b9f118ae2d5ecdb3132fc20ede3dd07d9e
Abel Pratama <79308095+Abelpratama@users.noreply.github.com>
2023-06-12 20:12:55 +0800

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./App.css";
+import HeaderComponent from "./components/headerComponent";
+import HomePage from "./pages/homePage";
+import DetailPage from "./pages/detailPage";
+import SavedPage from "./pages/savedPage";
+import SearchPage from "./pages/searchPage";
+
 function App() {
   return (
-    <div>
-      <div className="text-red-500">test</div>
-    </div>
+    <BrowserRouter>
+      <HeaderComponent />
+      <Routes>
+        <Route path="/" element={<HomePage />}></Route>
+        <Route path="/detail" element={<DetailPage />}></Route>
+        <Route path="/saved" element={<SavedPage />}></Route>
+        <Route path="/search/:query" element={<SearchPage />}></Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
