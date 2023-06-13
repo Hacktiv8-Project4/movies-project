9227f4e3ee3c90b0809f8c67994647eed057eb5e
Abel Pratama <79308095+Abelpratama@users.noreply.github.com>
2023-06-13 18:21:49 +0800

    Add files via upload

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  content: [],
   theme: {
-    extend: {
-      gridTemplateRows: {
-        // Simple 8 row grid
-        "custom-row": "repeat(3, max-content)",
-      },
-    },
+    extend: {},
   },
   plugins: [],
-};
+}
+
