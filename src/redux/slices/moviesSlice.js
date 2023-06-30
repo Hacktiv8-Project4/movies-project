640e47e5119ac291e068faf50c5c6b2d2b41e74c
ddnamn <notimportantfile931900@gmail.com>
2023-06-30 22:24:34 +0700

    finish

--- src/redux/slices/moviesSlice.js
+++ src/redux/slices/moviesSlice.js
@@ -10,7 +10,7 @@ const initialState = {
 
 export const fetchMovies = createAsyncThunk("fetchMoviesList", async () => {
   const response = await axios.get(
-    `${process.env.REACT_APP_BASE_API}3/discover/movie?api_key=${process.env.REACT_APP_X}`
+    `${process.env.REACT_APP_BASE_API}3/discover/movie?api_key=236242cfb53f88ab493d8d87d7f64257`
   );
   return response.data.results;
 });
@@ -19,7 +19,7 @@ export const searchMovies = createAsyncThunk(
   "fetchSearchMovies",
   async (searchTerm) => {
     const response = await axios.get(
-      `${process.env.REACT_APP_BASE_API}3/search/movie?query=${searchTerm}&api_key=${process.env.REACT_APP_X}`
+      `${process.env.REACT_APP_BASE_API}3/search/movie?query=${searchTerm}&api_key=236242cfb53f88ab493d8d87d7f64257`
     );
     return response.data.results;
   }
